19755f46b44e32c5b135f3263a08d3befc836a64
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-07-22 14:43:36 +0200

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -6,4 +6,4 @@ python main_finetune.py \
     --blr 1e-3 --layer_decay 0.95 \
     --weight_decay 0.05 --drop_path 0.1 --reprob 0.25 --mixup 0.8 --cutmix 1.0 \
     --data_path /local00/bioinf/imagenet1k \
-    --device 3
+    --device cuda:3
