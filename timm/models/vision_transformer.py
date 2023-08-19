4ac6b9716e4acc01466496a82d6139d3a8fcd3d1
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-08-19 11:50:23 +0200

    Update vision_transformer.py

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -20,6 +20,7 @@ for some einops/einsum fun
 
 Hacked together by / Copyright 2020 Ross Wightman
 """
+import einops
 import torch
 import torch.nn as nn
 from functools import partial
@@ -122,6 +123,38 @@ class Attention(nn.Module):
         x = self.proj_drop(x)
         return x
 
+class NativeFlashAttention(nn.Module):
+    """ timm.models.vision_transformer.Attention but with scaled_dot_product_attention """
+
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        assert hasattr(F, "scaled_dot_product_attention")
+        assert attn_drop == 0 and proj_drop == 0, "F.scaled_dot_product_attention dropout has no train/eval mode"
+        assert dim % num_heads == 0, "dim should be divisible by num_heads"
+        self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        q, k, v = einops.rearrange(
+            self.qkv(x),
+            "bs seqlen (three num_heads head_dim) -> three bs num_heads seqlen head_dim",
+            three=3,
+            num_heads=self.num_heads,
+            head_dim=self.head_dim,
+        ).unbind(0)
+
+        x = F.scaled_dot_product_attention(q, k, v)
+
+        x = einops.rearrange(x, "bs num_heads seqlen head_dim -> bs seqlen (num_heads head_dim)")
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
 
 class Block(nn.Module):
 
@@ -129,7 +162,7 @@ class Block(nn.Module):
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
         super().__init__()
         self.norm1 = norm_layer(dim)
-        self.attn = Attention(
+        self.attn = NativeFlashAttention(
             dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
